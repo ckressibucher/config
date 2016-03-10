6efaae2e3f8354bab117d4581454c69720a2a0a1
Claudio Kressibucher <ckressibucher@graphicworks.ch>
2016-03-10 11:30:31 +0100

    Helper: make argument `ArrayMergerInterface` optional

--- src/Helper.php
+++ src/Helper.php
@@ -3,13 +3,18 @@
 
 namespace Ckr\Config;
 
-
 class Helper
 {
 
     protected $arrayMerger;
 
-    public function __construct(ArrayMergerInterface $arrMerger)
+    /**
+     * If null, the default array merger is used.
+     * @see DefaultArrayMerger
+     *
+     * @param ArrayMergerInterface|null $arrMerger
+     */
+    public function __construct(ArrayMergerInterface $arrMerger = null)
     {
         $this->arrayMerger = $arrMerger;
     }
@@ -24,21 +29,24 @@ class Helper
     public function mergeArrays()
     {
         $arrays = func_get_args();
-        return call_user_func_array([$this->arrayMerger, 'mergeRecursively'], $arrays);
+        return ($this->arrayMerger)
+            ? call_user_func_array([$this->arrayMerger, 'mergeRecursively'], $arrays)
+            : DefaultArrayMerger::mergeAllRecursively($arrays);
     }
 
     /**
      * Merge `Config` objects. The later objects take precedence over
      * the former objects.
      *
-     * @param ...Config A list of Config objects
+     * @param ...Config A list of Config objects. The later have higher priority
      * @return Config
      */
     public function mergeConfig()
     {
         $configs = func_get_args();
-        $arrays = array_map(function ($cfg) { return $cfg->getConfig(); }, $configs);
-        $merged = call_user_func_array([$this->arrayMerger, 'mergeRecursively'], $arrays);
-        return new Config($merged);
+        $arrays = array_map(function (Config $cfg) {
+            return $cfg->getConfig();
+        }, $configs);
+        return new Config(call_user_func_array([$this, 'mergeArrays'], $arrays));
     }
 }
